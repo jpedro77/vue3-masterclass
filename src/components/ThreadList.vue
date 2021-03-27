da38619309a2ef3d4f85c48dd90188b16888c2f2
João Pedro <jpedrofernandes77@gmail.com>
2021-03-27 19:43:52 -0300

    [add] Criaçao dos componentes - home, lista dos topicos, pagina nao encontrada

---
+++ src/components/ThreadList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="col-full">
+
+    <div class="thread-list">
+
+      <h2 class="list-title">Threads</h2>
+
+      <thread-list-item v-for="thread in threads" :key="thread.id" :thread="thread" class="thread"/>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+import ThreadListItem from '@/components/ThreadListItem'
+
+export default {
+  components: {
+    ThreadListItem
+  },
+  props: {
+    threads: {
+      required: true,
+      type: Array
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import PageHome from '@/components/PageHome'
+import PageThreadShow from '@/components/PageThreadShow'
+import PageNotFound from '@/components/PageNotFound'
+import { createRouter, createWebHistory } from 'vue-router'
+import sourceData from '@/data.json'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: PageHome
+  },
+  {
+    path: '/thread/:id',
+    name: 'ThreadShow',
+    component: PageThreadShow,
+    props: true,
+    beforeEnter (to, from, next) {
+      // check if thread exists
+      const ThreadExists = sourceData.threads.find(thread => thread.id === to.params.id)
+      // if exists continue
+      if (ThreadExists) {
+        next()
+      } else {
+        next({
+          name: 'NotFound',
+          params: { pathMatch: to.path.substring(1).split('/') },
+          query: to.query,
+          hash: to.hash
+        })
+      }
+      // if doesnt exists redirect to not found
+    }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: PageNotFound
+  }
+]
+
+export default createRouter({
+  history: createWebHistory(),
+  routes
+})
